eb3a921dcd154bc9d31216ee3cf5590cd692bdda
sahilkamalny <sahilkamalny@gmail.com>
2025-03-05 23:45:21 -0500

    Placed fxml file in its own directory under resources

--- src/main/java/org/example/cardgame24/HelloApplication.java
+++ src/main/java/org/example/cardgame24/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml")); // loads fxml file
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/hello-view.fxml")); // loads fxml file
         Scene scene = new Scene(fxmlLoader.load(), 900, 600); // loads scene from fxml file
         stage.setTitle("Card Game - 24"); // title of stage
         stage.setScene(scene); // sets the scene for the stage
